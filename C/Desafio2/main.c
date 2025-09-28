5417e63b9c2aecb1508d28670d5d426b199b6c60
Wesley Silva <wesley.horquen@outlook.com>
2025-09-28 08:52:57 -0400

    Primeiros desafios

---
+++ C/Desafio2/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main(){
+    int A, B, soma;
+    scanf("%i", &A);
+    scanf("%i", &B);
+    soma = A + B;
+    printf("X = %i\n", soma);
+    return 0;
+}
---
+++ C/README.mk
@@ -0,0 +1,7 @@
+# Aqui vou estar colocando a resolução dos desafios em linguagem C 👨‍💻
+## Desafio 1:
+	- Hello World!
+	Como toda linguagem de programação, o primeiro passo a se dar é criar o famoso "Hello World!".
+## Desafio 2:
+	- Extremamente Básico:
+	Exemplo simples de uma soma.
